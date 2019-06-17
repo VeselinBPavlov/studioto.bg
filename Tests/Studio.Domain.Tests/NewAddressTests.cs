87944ae06d229899b824c900217efc943e93b224
VeselinBPavlov <veselin.b.pavlov@gmail.com>
2019-06-17 07:18:10 +0300

    Added Domain tests new project.

---
+++ Tests/Studio.Domain.Tests/NewAddressTests.cs
@@ -0,0 +1,31 @@
+namespace Studio.Domain.Tests
+{
+    using Xunit;
+
+    using Entities;
+    
+    public class NewAddressTests
+    {
+        [Fact]
+        public void NewAddressTestValid()
+        {
+            var address = new Address
+            {
+                Id = 1,
+                Apartment = "22",
+                Floor = "2",
+                Number = "3Ж",
+                Street = "Васил Левски",
+                District = null,
+                PostalCode = null,
+                Latitude = 40.545M,
+                Longitude = 40.214M,
+                CityId = 1,
+                ClientId = null,
+                LocationId = null
+            };
+
+            Assert.NotNull(address);
+        }
+    }
+}
